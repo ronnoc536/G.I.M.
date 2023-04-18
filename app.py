818da52227acbe9b28357ba927a07350df21ecba
Connor Marler <connormarler536@gmail.com>
2023-04-18 14:32:26 -0500

    made home page look a bit nicer

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 
 app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 app.secret_key = "hello"
 app.permanent_session_lifetime = timedelta(minutes=5)
 
@@ -17,11 +18,14 @@ def CheckRet():
 
 @app.route('/add_tool_page', methods=["POST", "GET"])
 def add_tool_page():
-  if request.method == "POST":#if they clicked submit on add tools for example
-                              # also where i think that the adding tool to db would go but idk
-    flash("Tool added successfully!") #lets user know that it got added
-    return redirect(url_for("user", from_page="add_tool_page")) # takes them back to where the options are
-  return render_template("add_tool_page.html")
+  if "user" in session:
+    if request.method == "POST":#if they clicked submit on add tools for example
+                                # also where i think that the adding tool to db would go but idk
+      flash("Tool added successfully!") #lets user know that it got added
+      return redirect(url_for("user")) # takes them back to where the options are
+    return render_template("add_tool_page.html")
+  else:
+    return redirect(url_for("user")) #send to user func where they will be told they are not logged in
     
 @app.route("/login", methods=["POST", "GET"])
 def login():
