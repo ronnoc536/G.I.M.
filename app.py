9a945c0b4fe8e58cf142817fdd73e93b56364d7a
Connor Marler <connormarler536@gmail.com>
2023-04-17 20:21:37 -0500

    added some buttons the house manager page

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def CheckRet():
     
 @app.route("/login", methods=["POST", "GET"])
 def login():
-  if "user" in session:
+  if "user" in session: #before anything, check if in login session
     flash("Already Logged In!") # let them know they are still logged in 
     return redirect(url_for("user")) # take them to the user page
   if request.method == "POST": #if password is submitted
@@ -30,17 +30,15 @@ def login():
       flash("House Manager Login Successful!") #let them know they got logged in
       return redirect(url_for("user")) #send them to the user page
     else:
-      flash("Invalid password!") # if still in the 
+      flash("Invalid password!")
       return redirect(url_for("login"))
   else:
     return render_template("login.html")
 
 def validate_password(password):
   # Perform password validation here
-  hashed_password = generate_password_hash("1107", method="sha256", salt_length=8)
+  hashed_password = generate_password_hash("1107247", method="sha256", salt_length=8)
   return check_password_hash(hashed_password, password)
-
-
 
 @app.route("/user")
 def user():
@@ -58,7 +56,6 @@ def logout():
     flash(f"You have been logged out, {user}", "info")
   session.pop("user", None)
   return redirect(url_for("login"))
-
 
 if __name__ == "__main__":
   app.run(debug = True)
